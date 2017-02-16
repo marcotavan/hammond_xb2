f48c46c845236901b6d1bb8703ae89645763859b
Marco Tavan <tavanm@vdagroup.com>
2017-02-16 20:51:14 +0100

    cambiato il drive delle porte nel keyboard scanner. Da strong a Open drain. Da testare

--- source/ButtonScanner.c
+++ source/ButtonScanner.c
@@ -11,6 +11,22 @@
 */
 
 #include "project.h"
+#include "common.h"
+#include "debug.h"
 #include "ButtonScanner.h"
 
+void ButtonScannerInit(void)
+{
+    buttonInputScan_S4_Write(1); // attiva PullUp
+    buttonInputScan_S5_Write(1); // attiva PullUp
+    buttonInputScan_S6_Write(1); // attiva PullUp
+    buttonInputScan_S7_Write(1); // attiva PullUp
+}
+
+void ButtonScannerPoll(void)
+{
+    static uint8 isButtonScannerInitialized = FALSE;
+    
+    
+}
 /* [] END OF FILE */
