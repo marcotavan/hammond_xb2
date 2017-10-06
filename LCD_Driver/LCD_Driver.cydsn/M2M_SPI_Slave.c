0665056282c3e506c512c09b8fd717cf1d5eba01
Marco Tavan <tavanm@vdagroup.com>
2017-10-06 13:57:15 +0200

    strutturati i dati ricevuti via SPI

--- LCD_Driver/LCD_Driver.cydsn/M2M_SPI_Slave.c
+++ LCD_Driver/LCD_Driver.cydsn/M2M_SPI_Slave.c
@@ -15,30 +15,50 @@
 #include "myLCD.h"
 #include "M2M_SPI_Slave.h"
 
-volatile uint8 spiData[160];
-struct display_data_t {
-	uint8 data[20];
-	
-};
+CY_ISR_PROTO(SS_ISR);
+#define MAX_DATA 20
+#define DIM_QUEUE 100
 
+uint8 rawData[DIM_QUEUE][MAX_DATA];
 volatile uint8 ptrIn = 0;
 uint8 ptrOut = 0;
 
-#define DIM_QUEUE 100
-struct display_data_t displayData[DIM_QUEUE];
+union lcdData_t {
+	struct display_data_t {
+		uint8 address;
+		uint8 len;
+		uint8 type;
+		uint8 val;
+		uint8 data[16];
+	} displayData;
+	
+	uint8 data[MAX_DATA];
+} LcdData;
 
+/*****************************************************************************\
+*  Funzione:     hasElements()                                                *
+*  Argomenti:    Nessuno                 				      				  *
+*  Restituisce:  Nessuno                                                      *
+*  Scopo:        ci sono elementi in coda se i due puntatori sono diversi     *
+\*****************************************************************************/
 uint8 hasElements(void) {
+	// ci sono elementi in coda se i due puntatori sono diversi
 	if (ptrIn != ptrOut) {
 		return (1);
 	} else {
 		return (0);
 	}
-	
 }
 
+/*****************************************************************************\
+*  Funzione:     isQueueNotFull()                                             *  	
+*  Argomenti:    Nessuno                 				      				  *
+*  Restituisce:  Nessuno                                                      *
+*  Scopo:        posso accodare se il successivo elemento e' libero		      *
+\*****************************************************************************/
 uint8 isQueueNotFull(uint8 pIn) {
 	uint8 val = (pIn + 1) % DIM_QUEUE; 
-	
+	// posso accodare se il successivo elemento e' libero
 	if(val != ptrOut) {
 		ptrIn = val;
 		return (1);
@@ -47,18 +67,20 @@ uint8 isQueueNotFull(uint8 pIn) {
 	}
 }
  
-
-CY_ISR_PROTO(SS_ISR);
-
+/*****************************************************************************\
+*  Funzione:     CY_ISR(SS_ISR)                                               *  	
+*  Argomenti:    Nessuno                 				      				  *
+*  Restituisce:  Nessuno                                                      *
+*  Scopo:        														      *
+\*****************************************************************************/
 CY_ISR(SS_ISR) { 
 	uint8 idx = 0;
 	// questo blocco costa 50uS
 	// Pin_SPIF_Write(1);	
 	if(isQueueNotFull(ptrIn)) {
-	
-		// accoda il dato all'indice  -> ptrIn 
-		for(idx = 0;idx<16;idx++) {
-			displayData[ptrIn].data[idx] = SPIS_M2M_ReadRxData();
+		// accoda il dato ricevuto alla struttura di indice -> ptrIn 
+		for(idx = 0;idx<MAX_DATA;idx++) {
+			rawData[ptrIn][idx] = SPIS_M2M_ReadRxData();
 		}
 		SPIS_M2M_ClearRxBuffer();
 		SPIS_M2M_ClearFIFO();
@@ -67,12 +89,24 @@ CY_ISR(SS_ISR) {
     Pin_SS_ClearInterrupt();
 }
 
+/*****************************************************************************\
+*  Funzione:     M2M_SPI_Init()                         	                  *  	
+*  Argomenti:    Nessuno                 				      				  *
+*  Restituisce:  Nessuno                                                      *
+*  Scopo:        														      *
+\*****************************************************************************/
 void M2M_SPI_Init(void) {
 	DBG_PRINTF("[%s]\n",__func__);
 	SPIS_M2M_Start();
 	isr_ss_1_StartEx(SS_ISR);
 }
 
+/*****************************************************************************\
+*  Funzione:     M2M_SPI_Slave_ApplicationPoll()							  *  	
+*  Argomenti:    Nessuno                 				      				  *
+*  Restituisce:  Nessuno                                                      *
+*  Scopo:        															  *
+\*****************************************************************************/
 void M2M_SPI_Slave_ApplicationPoll(void) {
 	static uint8 isInitialized = 0;
 	uint8 len = 0;
@@ -107,15 +141,18 @@ void M2M_SPI_Slave_ApplicationPoll(void) {
 
 		ptrOut++;	// primo elemento da prelevare
 		ptrOut %= DIM_QUEUE; 
+		memcpy(LcdData.data,rawData[ptrOut],MAX_DATA);
 		
 		DBG_PRINTF("%02d: ",ptrOut); // posizione del rpimo elemento
-		DBG_PRINT_ARRAY(displayData[ptrOut].data,16);
+		DBG_PRINT_ARRAY(LcdData.data,MAX_DATA);
 		DBG_PRINTF("\n");
 
+		
+		
 		// queste due funzioni a seguire impiegano circa 2ms
 		//Pin_SPIF_Write(1);	
-		myLCD_Position(0,0);  
-		myLCD_PrintString(displayData[ptrOut].data);
+		// myLCD_Position(0,0);  
+		// myLCD_PrintString(rawData[ptrOut]);
 		// Pin_SPIF_Write(0);	
 	}
 	
